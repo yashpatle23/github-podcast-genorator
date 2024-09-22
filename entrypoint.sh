4c7fa2aeda79536a48402feada09863e7e779837
yash patle <yashpatle.job@gmail.com>
2024-09-22 09:44:50 +0000

    error

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
+echo "==================="
 
-echo "Starting the application"
-
-git config --global user.name "${GITHUB ACTOR}"
+git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
-git config --global--add safe.directory /github/workspace
+git config --global --add safe.directory /github/workspace
 
-python3 /user/bin/feed.py
+python3 /usr/bin/feed.py
 
-git add -A && git commit -m "Update feed" 
-git push --set-upstream origin master
+git add -A && git commit -m "Update Feed"
+git push --set-upstream origin main
 
-echo "==============="
+echo "==================="
